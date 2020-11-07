d03309e427003005de6511a9a18b87603fea4226
chris shino <chris.shino@hotmail.com>
2020-11-07 13:43:13 -0500

    done 3 for real lol

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from 'react'
+import { useState } from "react";
 import styled from "styled-components";
 import Header from "./Header";
 import Button from "./Button";
@@ -7,7 +7,7 @@ import Deadman from "./DeadMan";
 import DeadLetters from "./DeadLetters";
 import TheWord from "./TheWord";
 import Keyboard from "./Keyboard";
-import words from "../data/words.json"
+import words from "../data/words.json";
 import GameOverModal from "./GameOverModal";
 
 import { colors, contentWidth } from "./GlobalStyles";
@@ -15,21 +15,29 @@ import { colors, contentWidth } from "./GlobalStyles";
 const initialGameState = { started: false, over: false, win: false };
 const App = () => {
   const [game, setGame] = useState(initialGameState);
-  const [word, setWord] = useState({str: ''});
+  const [word, setWord] = useState({ str: "" });
   const handleStart = () => {
-    setGame({...game, started: !game.started });
-    getNewWord()
-  }
+    setGame({ ...game, started: !game.started });
+    {
+      !word.str && getNewWord();
+    }
+  };
   const getNewWord = () => {
-    const randomWord = Math.floor(Math.random() * words.length)
-    setWord({str: words[randomWord]})
+    const randomWord = Math.floor(Math.random() * words.length);
+    setWord({ str: words[randomWord] });
+  };
+  let buttonText = "Start";
+  if (game.over || game.win === true) {
+    buttonText = "Clear";
+  } else if (game.started === true) {
+    buttonText = "Continue";
   }
   return (
     <Wrapper>
       {/* <GameOverModal /> */}
       <Header />
       <Nav>
-        <Button onClickFunc={handleStart}>Start</Button>
+        <Button onClickFunc={handleStart}>{buttonText}</Button>
         <Button>btn 2</Button>
       </Nav>
       {game.started && (
